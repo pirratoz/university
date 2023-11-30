b7ad1f6f5e223d46fe5c05d7d42be554d02e24ed
PirraToZ <pirratoz@gmail.com>
2023-12-01 02:06:54 +0300

    refactor(): remove ru locale

--- ics/fsm/utils/forms.py
+++ ics/fsm/utils/forms.py
@@ -2,7 +2,7 @@ fsm_str = \
 "A = {A}\n" \
 "W = {W}\n" \
 "Z = {Z}\n" \
-"Данные для кодирования:\n" \
+"Data for coding:\n" \
 "I = log2 (A) = {I} ({IQ})\n" \
 "L = log2 (Z) = {L} ({LX})\n" \
 "N = log2 (W) = {N} ({NY})"
