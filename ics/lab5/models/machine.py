b905367acf87f3f3d739dd17bdc2222b571505bf
PirraToZ <pirratoz@gmail.com>
2023-12-04 04:03:26 +0300

    refactor(): create pattern for create tables

--- ics/lab5/models/machine.py
+++ ics/lab5/models/machine.py
@@ -25,40 +25,36 @@ class Machine:
         self.trigger = trigger
 
 
-    def get_tables_exits(self) -> list[Fsm]:
+    def __get_tabels_pattern(self, size_vector_func: int, vector_headers: list[str], states_or_exits: list[list[str]]) -> list[Fsm]:
         tabels = []
         size_vector_state = len(self.structural_machine.headers[0])
         size_vector_inputs = len(self.structural_machine.inputs[0])
         headers = get_code_gray(size_vector_state)
 
-        for index_exit in range(len(self.structural_machine.exits[0][0])):
+        for index_func in range(size_vector_func):
             fsm = deepcopy(self.structural_machine)
             fsm.headers = headers
             fsm.exits = []
             fill_zero_states(fsm)
-            for current_index, value_header in enumerate(self.structural_machine.headers):
+
+            for current_index, value_header in enumerate(vector_headers):
                 index_header_map = fsm.headers.index(value_header)
                 for index_i in range(size_vector_inputs + 1):
-                    fsm.states[index_i][index_header_map] = self.structural_machine.exits[index_i][current_index][index_exit]
+                    fsm.states[index_i][index_header_map] = states_or_exits[index_i][current_index][index_func]
             tabels.append(fsm)
         
         return tabels
+
+    def get_tables_exits(self) -> list[Fsm]:
+        return self.__get_tabels_pattern(
+            len(self.structural_machine.exits[0][0]),
+            self.structural_machine.headers,
+            self.structural_machine.exits
+        )
 
     def get_tables_memory(self) -> list[Fsm]:
-        tabels = []
-        size_vector_state = len(self.structural_machine.headers[0])
-        size_vector_inputs = len(self.structural_machine.inputs[0])
-        headers = get_code_gray(size_vector_state)
-
-        for index_memory in range(len(self.memory_machine.states[0][0])):
-            fsm = deepcopy(self.structural_machine)
-            fsm.headers = headers
-            fsm.exits = []
-            fill_zero_states(fsm)
-            for current_index, value_header in enumerate(self.memory_machine.headers):
-                index_header_map = fsm.headers.index(value_header)
-                for index_i in range(size_vector_inputs + 1):
-                    fsm.states[index_i][index_header_map] = self.memory_machine.states[index_i][current_index][index_memory]
-            tabels.append(fsm)
-        
-        return tabels
+        return self.__get_tabels_pattern(
+            len(self.memory_machine.states[0][0]),
+            self.memory_machine.headers,
+            self.memory_machine.states
+        )
