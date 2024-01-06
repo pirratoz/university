03ae9d15f3d0c97030a80b39dec6bb7f89233d76
PirraToZ <pirratoz@gmail.com>
2024-01-06 20:14:23 +0300

    feat(): add import module

---
+++ ics/lab4/utils/__init__.py
@@ -0,0 +1,28 @@
+from .func import (
+    Bool,
+    TabelFunction,
+    TabelRow
+)
+
+from .row_generator import (
+    FillFinishedResult,
+    CalculationsResult
+)
+
+from .multiplexor import (
+    InputHashMap,
+    MultiplexorCountInput,
+    Multiplexor
+)
+
+
+__all__ = [
+    "Bool",
+    "TabelFunction",
+    "TabelRow",
+    "FillFinishedResult",
+    "CalculationsResult",
+    "InputHashMap",
+    "MultiplexorCountInput",
+    "Multiplexor"
+]
