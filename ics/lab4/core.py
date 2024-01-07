58f8186fcec3364fb0f327a98b6f88afda581126
PirraToZ <pirratoz@gmail.com>
2024-01-07 22:11:02 +0300

    feat(): stable +/- version

--- ics/lab4/core.py
+++ ics/lab4/core.py
@@ -1,42 +1,30 @@
 from pprint import pprint
 
 from utils import (
-    Bool,
     Multiplexor,
     MultiplexorCountInput,
-    ReadyResultsPlaceholder,
-    CalculatedResultsPlaceholder
+    RowPlaceholder
 )
 
+from variants import variants
 
-# This is an example of equivalent multiplexers
-
-# One is set using the function results
-ready_results = ReadyResultsPlaceholder(
-    count_var=5,
-    results=[0, 1, 2, 3, 4, 6, 7, 12, 16, 18, 20, 22, 28],
-    bool=Bool.true
-)
-# The other is set by the function itself
-calculations_results = CalculatedResultsPlaceholder(
-    function="!a * !b * !c + c * !d * !e + a * !b * !e + !a * !b * d"
-)
 
 mx = Multiplexor(
     count_input=MultiplexorCountInput.four,
-    row_placeholder=ready_results
+    row_placeholder=RowPlaceholder()
 )
 
-first = mx.inputs.dump()
-mx.row_placeholder = calculations_results
-second = mx.inputs.dump()
-
-print("========================================", "MX: Based on the results of the function", sep="\n")
-pprint(first, sort_dicts=False)
-print("========================================", "MX: By function", sep="\n")
-pprint(second, sort_dicts=False)
-print("========================================")
-print("Results equals: ", first == second)
+for ready_result, calculation_result in variants:
+    mx.row_placeholder = ready_result
+    first = mx.inputs.dump()
+    mx.row_placeholder = calculation_result
+    second = mx.inputs.dump()
+    print("========================================", "MX: Based on the results of the function", sep="\n")
+    pprint(first, sort_dicts=False)
+    print("========================================", "MX: By function", sep="\n")
+    pprint(second, sort_dicts=False)
+    print("========================================")
+    print("Results equals: ", first == second, "\n\n")
 
 # Attention!! 
 
